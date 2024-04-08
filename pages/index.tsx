f8824e141804af9e914bcb215b2962b2a3f0b10f
Phamlamthai <105089922+Phamlamthai@users.noreply.github.com>
2024-04-08 20:21:31 +0700

    initial project

---
+++ pages/index.tsx
@@ -0,0 +1,44 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.scss";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+import axios from "axios";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function home({ country }) {
+  // console.log({ country });
+  return (
+    <>
+      <Header country={country} />
+      <div className={styles.home}></div>
+      <Footer />
+    </>
+  );
+}
+export async function getServerSideProps() {
+  // db.connectDb();
+  // let products = await Product.find().sort({ createdAt: -1 }).lean();
+  let data = await axios
+    .get("https://api.ipregistry.co/?key=5pkrc8g2c4pib5mi")
+    .then((res) => {
+      return res.data.location.country;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  // console.log({ data });
+
+  return {
+    props: {
+      // products: JSON.parse(JSON.stringify(products)),
+      // country: { name: data.name, flag: data.flag.emojitwo },
+      country: {
+        name: "Viet Nam",
+        flag: "https://cdn-icons-png.flaticon.com/128/197/197473.png?w=360",
+      },
+    },
+  };
+}
